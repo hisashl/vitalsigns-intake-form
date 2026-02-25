3bf856f05a9333065b99fa9f8ac30257e7d9f129
hisashl <149032921+hisashl@users.noreply.github.com>
2026-02-24 18:02:30 -0600

    cahnges

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
- 
+
 app.use(cors());
 app.use(express.json());
- 
-app.use(express.static('public'));
- 
+
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.post('/api/register-patient', (req, res) => {
     const patientData = req.body;
     console.log("New Patient Received:", patientData);
     res.status(200).send({ message: "Registration successful!" });
 });
+
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
